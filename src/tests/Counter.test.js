a527838db7058505e8d71e30310a002439f46a99
Joseph Schley <113475542+Jospasan@users.noreply.github.com>
2024-04-08 04:11:44 -0400

    Updating the unit test code
    Updated counter.test.js to have test code to have 100% for all test coverage.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,37 @@
 // import necessary react testing library helpers here
+import { render, screen, fireEvent } from '@testing-library/react';
 // import the Counter component here
+import Counter from '../components/Counter';
+
+//tests
+let getByText, getByTestId;
 
 beforeEach(() => {
   // Render the Counter component here
-})
+  const component = render(<Counter />);
+  getByText = component.getByText;
+  getByTestId = component.getByTestId;
+});
 
-test('renders counter message', () => {
+test('renders counter message', () => { //the message is "Counter"
   // Complete the unit test below based on the objective in the line above
+  expect(getByText(/Counter/i)).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(getByTestId('count')).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(getByText('+'));
+  expect(getByTestId('count')).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(getByText('+')); // Increment first to avoid negative count
+  fireEvent.click(getByText('-'));
+  expect(getByTestId('count')).toHaveTextContent('0');
 });
